f76f84f5ef9a9a778a6119c38535cb1192fa61b2
Jeroen <Jeroen_vanvoorst@hotmail.com>
2018-06-03 20:23:05 +0200

    cleared up the interface

--- app/src/main/java/com/example/pascal/apitest/PrefAct.java
+++ app/src/main/java/com/example/pascal/apitest/PrefAct.java
@@ -13,22 +13,15 @@ import com.example.pascal.apitest.activities.UserlistAct;
 import java.util.List;
 
 public class PrefAct extends AppCompatActivity {
-    private EditText lastfmuser, fmdeleteuser;
-    private Button deleteall, manageplaylists, manageusers;
+    private Button manageplaylists, manageusers;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pref);
-        deleteall = findViewById(R.id.deleteall_btn);
 //        playlist =  findViewById(R.id.txt_playlist);
         manageplaylists = findViewById(R.id.manage_playlists);
         manageusers = findViewById(R.id.manage_users);
-        deleteall.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                delete();
-            }
-        });
         manageplaylists.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 Intent i = new Intent(PrefAct.this, PlaylistAct.class);
@@ -45,10 +38,5 @@ public class PrefAct extends AppCompatActivity {
 
     }
 
-    private void deleteUser() {
-        String user = fmdeleteuser.getText().toString();
-    }
 
-    private void delete() {
-    }
 }
